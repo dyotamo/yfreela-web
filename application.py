56fff1a1896a5e67d926b354b83a8a5dcbcbdc4d
Dássone J <dyotamo@gmail.com>
2020-05-26 16:39:49 +0200

    Adding PL generation scripts and giving back search util.

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ class CategoryDetailsResource(object):
     def on_get(self, req, resp, name):
         resp.body = json.dumps([
             freela.__dict__ for freela in FREELAS
-            if (name.lower() in freela.category.lower())
+            if (freela.category.lower() == name.lower())
         ])
 
 
@@ -26,8 +26,17 @@ class FreelaDetailsResource(object):
         resp.status = falcon.HTTP_404
 
 
+class SearchResource(object):
+    def on_get(self, req, resp, query):
+        resp.body = json.dumps([
+            freela.__dict__ for freela in FREELAS
+            if (query.lower() in freela.category.lower())
+        ])
+
+
 application = falcon.API()
 
 application.add_route('/categories', CategoryResource())
 application.add_route('/categories/{name}', CategoryDetailsResource())
 application.add_route('/freelas/{id}', FreelaDetailsResource())
+application.add_route('/search/{query}', SearchResource())
