5b2f5d33c343060f2886d0c9ca74a3034e2d71fd
Dássone J <dyotamo@gmail.com>
2020-06-23 18:02:57 +0200

    Impedir o like e dislike no mesmo freela.

--- services.py
+++ services.py
@@ -18,18 +18,29 @@ class FreelaService:
         return Freela.select().where(Freela.category.contains(cat_query))
 
     def like_or_dislike_freela(self, freela_id, device_id, action):
+        freela = self.get_freela(freela_id)
+
         if action == 'like':
-            like_or_dislike, created = Like.get_or_create(
-                freela=self.get_freela(freela_id), device_id=device_id)
+            like_or_dislike, created = Like.get_or_create(freela=freela,
+                                                          device_id=device_id)
+
+            # Não pode dar like e deslike ao mesmo tempo, então depois de dar like deve remover o dislike, se existir...
+            if self.disliked(freela, device_id):
+                Dislike.get(freela=freela,
+                            device_id=device_id).delete_instance()
+
         elif action == 'dislike':
             like_or_dislike, created = Dislike.get_or_create(
-                freela=self.get_freela(freela_id), device_id=device_id)
+                freela=freela, device_id=device_id)
 
-        if created:
-            return self.get_freela(freela_id)
-        else:
+            if self.liked(freela, device_id):
+                Like.get(freela=freela, device_id=device_id).delete_instance()
+
+        # Se o like ou dislike sempre existiu
+        if created == False:
             like_or_dislike.delete_instance()
-            return self.get_freela(freela_id)
+
+        return self.get_freela(freela_id)
 
     def liked(self, freela, device_id):
         return Like.get_or_none(freela=freela, device_id=device_id) is not None
